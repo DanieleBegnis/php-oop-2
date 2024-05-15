6aa79bc6dcd7416c346c51a3e5ada85277dde0b9
DanieleBegnis <daniele.begnis@gmail.com>
2024-05-15 15:58:16 +0200

    feat: add rough card with name and description

--- Models/Category.php
+++ Models/Category.php
@@ -1,7 +1,14 @@
 <?php
-class Category {
+class Category
+{
     public $name;
     public $type;
     public $description;
+
+    public function __construct($_name, $_type, $_description)
+    {
+        $this->name = $_name;
+        $this->type = $_type;
+        $this->description = $_description;
+    }
 }
-?>
--- index.php
+++ index.php
@@ -5,20 +5,53 @@
 Stampiamo delle card contenenti i dettagli dei prodotti, 
 come immagine, titolo, prezzo, icona della categoria ed 
 il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia). -->
-<?php 
+<?php
 require_once __DIR__ . '/Models/Category.php';
 require_once __DIR__ . '/Models/Dog.php';
 require_once __DIR__ . '/Models/Cat.php';
+
+$ciotola = new Category('Pippolo', 'Ciotola', 'Lorem ipsum');
+$osso = new Category('Ossolo', 'osso', 'Lorem ipsum');
+$guinzaglio = new Category('Guinzagliolo', 'guinzaglio', 'Lorem ipsum');
+$palla = new Category('Pallolo', 'palla', 'Lorem ipsum');
+$croccantini = new Category('Croccantinolo', 'croccantini', 'Lorem ipsum');
+
+$products = [
+    $ciotola,
+    $osso,
+    $guinzaglio,
+    $palla,
+    $croccantini
+];
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>WebStore</title>
 </head>
+
 <body>
-    test test
+    <div class="container">
+        <div class="row">
+            <?php foreach ($products as $product) { ?>
+                <div class="col-3">
+                    <div class="card my-3" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $product->name; ?></h5>
+                            <p class="card-text"><?php echo $product->description; ?></p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
 </body>
+
 </html>
